d1bf80a3e363856d31e4ed315dfbfc46cba3dc05
dharman01kothari <65413671+dharman01kothari@users.noreply.github.com>
2020-06-09 19:45:39 -0400

    Upto part 2
    Halfway there

--- get_data.py
+++ get_data.py
@@ -1,4 +1,25 @@
 # get_data.py
 import requests
+import json
 
-print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+if __name__=="__main__":
+    print("REQUESTING SOME DATA FROM THE INTERNET...")
+    request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+    
+    response = requests.get(request_url)
+    response_data = json.loads(response.text)
+    
+    print(response_data['name'])
+
+    request2_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products.json"
+
+    resp = requests.get(request2_url)
+    resp_data = json.loads(resp.text)
+
+    #print(type(resp_data))
+    i = 0
+    
+    while i != len(resp_data):
+        print('Name:',resp_data[i]['name'],' ','ID:',resp_data[i]['id'])
+        i = i + 1
